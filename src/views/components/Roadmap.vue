2a86c33894d122a93ca6dfb72b8858d0f0e29287
Oguzhan <oguzhanumut@icloud.com>
2022-02-03 02:00:32 +0100

    first commit

---
+++ src/views/components/Roadmap.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="roadmap">
+    <div class="container">
+      <div class="timeline">
+        <div class="timeline-container warning">
+          <div class="timeline-icon">
+            <i class="fa fa-pencil" aria-hidden="true"></i>
+          </div>
+          <div class="timeline-body">
+            <h4 class="timeline-title"><span class="badge">Art Generation</span></h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              necessitatibus numquam earum ipsa fugiat veniam suscipit, officiis
+              repudiandae, eum recusandae neque dignissimos. Cum fugit
+              laboriosam culpa, repellendus esse commodi deserunt.
+            </p>
+            <p class="timeline-subtitle">Early February</p>
+          </div>
+        </div>
+        <div class="timeline-container warning">
+          <div class="timeline-icon">
+            <i class="fa fa-code" aria-hidden="true"></i>
+          </div>
+          <div class="timeline-body">
+            <h4 class="timeline-title"><span class="badge">Candy Machine</span></h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              necessitatibus numquam earum ipsa fugiat veniam suscipit, officiis
+              repudiandae, eum recusandae neque dignissimos. Cum fugit
+              laboriosam culpa, repellendus esse commodi deserunt.
+            </p>
+            <p class="timeline-subtitle">Mid February</p>
+          </div>
+        </div>
+        <div class="timeline-container warning">
+          <div class="timeline-icon">
+            <i class="fa fa-book" aria-hidden="true"></i>
+          </div>
+          <div class="timeline-body">
+            <h4 class="timeline-title"><span class="badge">Launch</span></h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              necessitatibus numquam earum ipsa fugiat veniam suscipit, officiis
+              repudiandae, eum recusandae neque dignissimos. Cum fugit
+              laboriosam culpa, repellendus esse commodi deserunt.
+            </p>
+            <p class="timeline-subtitle">Late February</p>
+          </div>
+        </div>
+        <div class="timeline-container warning">
+          <div class="timeline-icon">
+            <i class="fa fa-rocket" aria-hidden="true"></i>
+          </div>
+          <div class="timeline-body">
+            <h4 class="timeline-title"><span class="badge">Secondary Listing</span></h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              necessitatibus numquam earum ipsa fugiat veniam suscipit, officiis
+              repudiandae, eum recusandae neque dignissimos. Cum fugit
+              laboriosam culpa, repellendus esse commodi deserunt.
+            </p>
+            <p class="timeline-subtitle">Early March</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
