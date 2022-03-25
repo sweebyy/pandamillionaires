2c5949bf3cf71ec995e805b946fa550252a90bea
SWEEBYY <MrSweeb@outlook.fr>
2022-03-25 09:33:49 +0100

    last

--- src/views/components/Roadmap.vue
+++ src/views/components/Roadmap.vue
@@ -7,12 +7,13 @@
             <i class="fa fa-pencil" aria-hidden="true"></i>
           </div>
           <div class="timeline-body toReveal">
-            <h4 class="timeline-title"><span class="badge">Art Generation</span></h4>
+            <h4 class="timeline-title"><span class="badge">PHASE 1 : Building The Foundations</span></h4>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-              necessitatibus numquam earum ipsa fugiat veniam suscipit, officiis
-              repudiandae, eum recusandae neque dignissimos. Cum fugit
-              laboriosam culpa, repellendus esse commodi deserunt.
+     Assemble a team of Developers and Community Managers ✔
+
+     Unveiling social networks to the public ✔
+
+     Build the Discord community and reward our most loyal members ✔
             </p>
             <p class="timeline-subtitle">Early February</p>
           </div>
